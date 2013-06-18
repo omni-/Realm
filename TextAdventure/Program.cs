713efdeace1547ec8030fd03007dce0ba0f51c07
Cooper Teixeira <cooper@stevetex.com>
2013-06-18 10:35:40 -0700

    Initial Commit

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home.Init();
+        }
+    }
+}
